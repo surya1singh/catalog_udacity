18964e5a3f5ba44c4bd6b4386735a241709d573c
vagrant <vagrant@vagrant.vm>
2018-06-21 13:36:55 +0000

    login page 1. simple login page template 2. working google auth button

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask import session as login_session
+import random
+import string
 app = Flask(__name__)
 
 
@@ -7,22 +10,17 @@ app = Flask(__name__)
 def HelloWorld():
     return "Hello World"
 
+# Create anti-forgery state token
+@app.route('/login')
+def showLogin():
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                    for x in xrange(32))
+    login_session['state'] = state
+    # return "The current session state is %s" % login_session['state']
+    return render_template('login.html', STATE=state)
+
+
 if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
